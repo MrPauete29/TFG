55ac200d936356b9182fa47278ede48d715c0750
MrPauete29 <1637556@uab.cat>
2025-03-16 22:45:45 +0100

    clean and split dataset

--- Datasets/ListaEscogidos/Clean_dataset.py
+++ Datasets/ListaEscogidos/Clean_dataset.py
@@ -20,24 +20,7 @@ def eliminate_row_na(df, threshold):
     df = df.dropna(axis=0, thresh=threshold)
     return df
 
-if __name__ == "__main__"
-    document = "Stroke.csv"
-    if document == None:
-        document = input("Document (Without extension: ")
-        document = f"{document}.csv"
 
-    try:
-        df = pd.read_csv(document)
-        print(df.shape)
-    except FileNotFoundError:
-        raise FileNotFoundError("Archivo no encontrado")
-    except:
-        raise ValueError("Error en la lectura del archivo")
-    if document == "Stroke.csv":
-        df['smoking_status'] = df['smoking_status'].replace('Unknown', np.nan)
-    df = eliminate_column_na(df, 70)
-    df = eliminate_row_na(df, 0)
-    print(df.shape)
 
 
 
--- Datasets/ListaEscogidos/Split_Dataset.py
+++ Datasets/ListaEscogidos/Split_Dataset.py
@@ -1,22 +1,9 @@
 import pandas as pd
 
-try:
-    df = pd.read_csv(document)
-    print(df)
-except FileNotFoundError:
-    raise FileNotFoundError("Archivo no encontrado")
-except:
-    raise ValueError("Error en la lectura del archivo")
 
-def split_dataset(target_column):
+def split_dataset(df,target_column):
     target_df = df[target_column]
     features_df = df.drop(columns=[target_column])
     return target_df,features_df
 
-if __name__ = "__main__":
-    document = input("Document (Without extension: ")
-    document = f"{document}.csv"
-    target_column = input("Name of the column you want to predict")
-    target_df,features_df = split_dataset(target_column)
-    target_df.to_csv(f"Target/{document}")
-    features_df.to_csv(f"Features/{document}")
+
