2fce90a75732462ebf3f873f27d8d27f377363e4
MrPauete29 <1637556@uab.cat>
2025-03-25 00:17:13 +0100

    xgboost model creation

--- development/ml_models/LogisticRegression.py
+++ development/ml_models/LogisticRegression.py
@@ -6,17 +6,21 @@ class LogisticRegression:
     def __init__(self, **params):
         self.model = lr(**params)
         self.trained = False
+
     def train(self, X_train: pd.DataFrame, y_train: pd.DataFrame):
         self.trained = True
         return self.model.fit(X_train, y_train)
+
     def predict(self, X_val):
         if not self.trained:
             raise Exception("Model not trained")
         return self.model.predict(X_val)
+
     def predict_proba(self, X_val):
         if not self.trained:
             raise Exception("Model not trained")
         return self.model.predict_proba(X_val)[:,1]
+
     def evaluate(self, X: pd.Dataframe = None, y: pd.DataFrame = None, y_pred: pd.DataFrame = None):
         if not self.trained:
             raise Exception("Model not trained")
@@ -27,7 +31,9 @@ class LogisticRegression:
                 raise ValueError("y_pred set or X set must be provided")
             y_pred = self.predict(X)
         return classification_report(y, y_pred)
+
     def save_model(self, filename: str):
         joblib.dump(self.model,f"trained_models/random_forest/{filename}")
+
     def load_model(self, filename: str):
         self.model = joblib.load(f"trained_models/random_forest/{filename}")
