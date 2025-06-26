8097739d282600d40f31f9be2456fd721350dcee
MrPauete29 <129769664+MrPauete29@users.noreply.github.com>
2025-06-26 22:08:25 +0200

    Delete Datasets directory

--- Datasets/ListaEscogidos/Split_Dataset.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-
-def split_dataset(df,target_column):
-    target_df = df[target_column]
-    features_df = df.drop(columns=[target_column])
-    return target_df,features_df
-
-
